b64199d3f15e71e3353f56f2a5e2512bdd9a7ac2
goggxi <goggxi@gmail.com>
2021-07-08 01:13:55 +0800

    add calculator proto - server - client

---
+++ src/main/java/com/goggxi/grpc/calculator/server/CalculatorServer.java
@@ -0,0 +1,27 @@
+package com.goggxi.grpc.calculator.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class CalculatorServer {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Server server = ServerBuilder.forPort(50053)
+                .addService(new CalculatorServiceImpl())
+//                .addService(ProtoReflectionService.newInstance()) // reflection
+                .build();
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received Shutdown Request");
+            server.shutdown();
+            System.out.println("Successfully stopped the server");
+        }));
+
+        server.awaitTermination();
+    }
+
+}
